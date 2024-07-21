78f87da1e98a5a56d69ab5032ddc0b29d787153b
SadraNoa <sadranoa@gmail.com>
2024-07-21 13:59:16 +0330

    first commit

---
+++ yolo_infrence.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+model = YOLO('models/best (3).pt')
+
+results = model.predict('input_videos/A1606b0e6_0 (14).mp4', save = True)
+print(results[0])
+print('*************************** ')
+for box in results[0].boxes :
+    print(box)
